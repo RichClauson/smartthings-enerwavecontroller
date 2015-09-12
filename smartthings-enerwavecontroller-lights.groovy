2b4eb89bb7eee4850744395c4b0a1e47db9b05b1
Jason Grey <jason@jason-grey.com>
2015-09-12 17:34:03 -0500

    initial versions

---
+++ smartthings-enerwavecontroller-lights.groovy
@@ -0,0 +1,97 @@
+/**
+ *  Set lights based on scene
+ *
+ *  Copyright 2014 Jason Grey
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "Set lights based on scene number",
+    namespace: "jt55401",
+    author: "Jason Grey",
+    description: "Turn lights on or off based on scene.",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+	section("When this scene controller reaches scene number...") {
+		input "sceneController", "capability.button", title: "Pick an Enerwave Scene Controller", required: true
+        input "sceneNumber", "enum", title: "Changes to Scene:", description: "scene number 1-7", required: true, options: ["1","2","3","4","5","6","7"]
+	}
+    section("Fiddle with these lights:") {
+    	input "offLights", "capability.switch", title:"Lights to turn OFF", multiple:true, required: false
+        input "onLights", "capability.switch", title:"Lights to turn ON", multiple:true, required: false
+        input "levelLights", "capability.switchLevel", title: "Lights to LEVEL", multiple:true, required:false
+        input "lev", "number", title: "Level", required:false, multiple:false, range: "(0..99)"
+    }
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+	// TODO: subscribe to attributes, devices, locations, etc.
+    subscribe(sceneController, "scene", sceneChanged)
+}
+
+def sceneChanged(evt){
+    def s = sceneController.latestValue("scene").toInteger()
+    def s2 = sceneNumber.toInteger()
+    
+    log.debug "the scene is now ${s}, looking for ${s2}"
+    
+    if( s == s2 ){
+    	
+        if( offLights != null ) offLights.off()
+        
+        if( onLights != null ) {
+        	onLights.each { l ->
+            	if( hasSetLevelCommand(l) ) {
+                	l.setLevel(99)
+                } else {
+                	l.on()
+                }
+            }
+        }
+        
+        if( levelLights != null ) {
+        	levelLights.each{ l ->
+            	l.setLevel( lev )
+            }
+        }
+        
+    }
+}
+
+private hasSetLevelCommand(device) {
+	def hasLevel = false
+	device.supportedCommands.each {
+		if (it.name.contains("setLevel")) {
+			hasLevel = true
+            log.debug "${device} has level..."
+		}
+	}
+	return hasLevel
+}
+
+// TODO: implement event handlers
